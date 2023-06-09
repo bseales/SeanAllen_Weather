11b6b5fe1655456e75125387afd6a5cfe625da4e
Batson Seales <batson.seales@gmail.com>
2023-06-09 18:06:23 -0500

    Initial Commit

---
+++ SeanAllen_Weather/SeanAllen_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeanAllen_WeatherApp.swift
+//  SeanAllen_Weather
+//
+//  Created by Batson Seales on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SeanAllen_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
